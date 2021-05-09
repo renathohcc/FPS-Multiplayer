9998cb501a4e6e355048bcf22152a87736cf9a7c
Renatho Henrique Campos das Chagas <renathohcc@hotmail.com>
2021-05-08 23:12:32 -0300

    [UPDATE] START PHOTON SETTINGS
    Bug with the connection

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    public static Launcher instance;
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    public GameObject loadingScreen;
+    public TMP_Text loadingText;
+    public GameObject menuButtons;
+    void Start()
+    {
+        CloseMenus();
+        //Connecting to the server
+        loadingScreen.SetActive(true);
+        loadingText.text = "Connecting to the server...";
+        //To connect with photon server
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Connected using settings ON");
+    }
+
+    public void CloseMenus(){
+        loadingScreen.SetActive(false);
+        menuButtons.SetActive(false);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        //Connecting to the lobby
+        PhotonNetwork.JoinLobby();
+        Debug.Log("Joined Lobby");
+        loadingText.text = "Joining in the Lobby";
+        
+    }
+
+    public override void OnJoinedLobby()
+    {
+        CloseMenus();
+        menuButtons.SetActive(true);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
